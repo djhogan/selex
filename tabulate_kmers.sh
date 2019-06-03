291d2703addfb6b325e4c946efd4c825d4644504
Daniel Hogan <djh6788@gmail.com>
2019-06-03 09:31:18 -0600

    clamp zero counts to 1

--- tabulate_kmers.sh
+++ tabulate_kmers.sh
@@ -4,6 +4,7 @@ do
   join -o auto -e 0 -a 1 -a 2 \
     <(perl count_kmers.pl $i <protein_r1 | sort) \
     <(perl count_kmers.pl $i <protein_r2 | sort) \
+    | awk '$2==0{$2=1}$3==0{$3=1}{print}' \
     | perl add_freq_column.pl \
     >kmers/$i
   echo $i finished
