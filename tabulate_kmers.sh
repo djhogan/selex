057341be24a1ad25c39d7f1e788ceef29f2bd2bd
Daniel Hogan <djh6788@gmail.com>
2019-06-03 16:48:13 -0600

    add usage and argument handling

--- tabulate_kmers.sh
+++ tabulate_kmers.sh
@@ -1,12 +1,59 @@
+#!/usr/bin/env bash
+
+usage(){
+  echo "Usage: $0 [-d DIR] FILE1 FILE2" 1>&2
+  exit 1
+}
+
+DIR="."
+
+while getopts "d:" o; do
+  case "$o" in
+    d)
+      DIR=$OPTARG
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+if [ ! -d "$DIR" ]
+then
+  echo "Directory $DIR does not exist"
+  exit 1
+fi
+
+if [ "$#" -ne 2 ]
+then
+  usage
+fi
+
+if [ ! -f "$1" ]
+then
+  echo "File $1 does not exist"
+  exit 1
+fi
+
+if [ ! -f "$2" ]
+then
+  echo "File $2 does not exist"
+  exit 1
+fi
+
+FILE1=$1
+FILE2=$2
+
 for i in `seq 5 15`
 do
   echo $i started
   join -o auto -e 0 -a 1 -a 2 \
-    <(perl count_kmers.pl $i <protein_r1 | sort) \
-    <(perl count_kmers.pl $i <protein_r2 | sort) \
+    <(perl count_kmers.pl $i <"$FILE1" | sort) \
+    <(perl count_kmers.pl $i <"$FILE2" | sort) \
     | awk '$2==0{$2=1}$3==0{$3=1}{print}' \
     | perl add_freq_column.pl \
-    >kmers/$i
+    >"${DIR}/${i}"
   echo $i finished
 done
 
