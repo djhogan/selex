9cc25b8d8b960eda3de5f31cc99dadfe382fd3df
Daniel Hogan <daniel.hogan@usask.ca>
2019-06-03 09:30:17 -0600

    update plotting script

--- plot_kmers.sh
+++ plot_kmers.sh
@@ -1,3 +1,2 @@
-awk '($3-$2)>=2{print}' \
-  | gnuplot -p -e 'set terminal svg mouse; plot "-" using 2:($3-$2):1 with labels
-  hypertext point pt 7 ps 0.5'
+awk '$5/($2>=1?$4:1)>=1.01{print}' \
+  | gnuplot -p -e 'set terminal svg mouse; plot "-" using 4:($5/$4):1 with labels hypertext point pt 7 ps 0.5'
